a2d7d115673b8373ec5ab1f1160f43b26b6d4739
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-05-07 17:06:56 -0300

    refact: implementing __enter__ and __exit__ magic methods to DBConnectionHandler

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
@@ -9,6 +10,7 @@ class DBConnectionHandler:
     def __init__(self):
         self.__connection_string = os.getenv("DATABASE_URL")
         self.__engine = self.__create_database_engine()
+        self.session = None
 
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
@@ -16,3 +18,11 @@ class DBConnectionHandler:
 
     def get_engine(self):
         return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
