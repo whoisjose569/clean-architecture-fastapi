6b45f7d51317bb3af1a8e9b9ff0c3c8a202bfeee
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-05-22 15:06:07 -0300

    refact: refact on class names

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -10,9 +10,7 @@ class UserRepository(IUsersRepository):
     def insert_user(cls, first_name: str, last_name: str, age: int) -> None:
         with DBConnectionHandler() as database:
             try:
-                new_registry = Users(
-                    first_name=first_name, last_name=last_name, age=age
-                )
+                new_registry = User(first_name=first_name, last_name=last_name, age=age)
                 database.session.add(new_registry)
                 database.session.commit()
             except Exception as exception:
@@ -24,8 +22,8 @@ class UserRepository(IUsersRepository):
         with DBConnectionHandler() as database:
             try:
                 registry_on_db = (
-                    database.session.query(Users)
-                    .filter(Users.first_name == first_name)
+                    database.session.query(User)
+                    .filter(User.first_name == first_name)
                     .all()
                 )
                 return registry_on_db
