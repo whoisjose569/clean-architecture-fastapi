0c658c3d8d162b76d3d2ef318f6c910bf13c1c3e
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-05-22 15:35:47 -0300

    feat: implementing user_register use_case

---
+++ src/data/use_cases/user_register.py
@@ -0,0 +1,24 @@
+from src.domain.use_cases.user_register import IUserRegister
+from src.domain.repositories.users_repository import IUsersRepository
+from src.domain.entities.user import User
+
+
+class UserRegister(IUserRegister):
+    def __init__(self, user_repository: IUsersRepository):
+        self.__user_repository = user_repository
+
+    def register(self, user: User):
+        self.__user_repository.insert_user(
+            first_name=user.first_name, last_name=user.last_name, age=user.age
+        )
+        response = {
+            "type": "Users",
+            "count": 1,
+            "attributes": {
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "age": user.age,
+            },
+        }
+
+        return response
