2b4004e994c9afcc0ad5d4e76219b1c3745edbe3
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-05-22 16:53:59 -0300

    feat: implementing pydantic schemas

---
+++ src/schemas/user_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, field_validator
+import re
+
+
+class UserSchemaIn(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+    @field_validator("first_name", "last_name")
+    @classmethod
+    def only_letters(cls, value: str):
+        if not re.fullmatch(r"[A-Za-zÀ-ÿ\s]+", value):
+            raise ValueError("Only letters and spaces are allowed")
+        return value
+
+
+class UserSchemaFindFirstName(BaseModel):
+    first_name: str
+
+    @field_validator("first_name")
+    @classmethod
+    def only_letters(cls, value: str):
+        if not re.fullmatch(r"[A-Za-zÀ-ÿ\s]+", value):
+            raise ValueError("Only letters and spaces are allowed")
+        return value
