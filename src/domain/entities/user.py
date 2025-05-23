141ba02012a1d3a8d1ad67510089f29385c19f29
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-05-23 17:08:56 -0300

    refact: refact on id attribute in User entity

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 class User:
     def __init__(
-        self, id: int, first_name: str, last_name: str, age: Optional[int] = None
+        self, first_name: str, last_name: str, age: int, id: Optional[int] = None
     ):
         self.id = id
         self.first_name = first_name
